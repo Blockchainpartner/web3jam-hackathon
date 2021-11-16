5f838531ee51105ef20bf304733aceedb65dd25c
hanahem <wawa.h8@gmail.com>
2021-11-16 13:55:38 +0100

    :construction: WIP - setting up integrations page

---
+++ components/IntegrationsScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const IntegrationsScreen = () => {
+  return (
+    <div className="w-full xl:w-5/6 m-auto my-16">
+      <div className="flex justify-between items-start">
+        <div className="flex flex-col items-start">
+          <h4 className="font-bold">{"Code Integrations"}</h4>
+          <p className="font-semibold text-sm text-subtxt">
+            {
+              "You can analyze your activity score and mint an NFT to access advantages"
+            }
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntegrationsScreen;
---
+++ pages/integrations.js
@@ -0,0 +1,9 @@
+import IntegrationsScreen from "../components/IntegrationsScreen";
+
+export default function Integrations() {
+  return (
+    <div className="flex flex-col justify-center">
+      <IntegrationsScreen />
+    </div>
+  );
+}
