762ffa6349e4588dbb9bf56371a956dc3b87efd6
hanahem <wawa.h8@gmail.com>
2021-11-17 13:53:03 +0100

    :sparkles: Adding unfinished mock integration

--- components/IntegrationsScreen.js
+++ components/IntegrationsScreen.js
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
-import { getLorem } from "../utils/utils";
+import { shadesOfPurple } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { getLorem, INTEGRATION_CODE } from "../utils/utils";
 
 function getScoreTile() {
-  const code =
-    '<button className="btn flex items-center">\n<img src="/logo-icon-white.png" alt="dyFactor" className="mr-2 h-4" />\n{"Access with dyFactor Score"}\n</button>';
+  const [hideContent, setHideContent] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const fakeUnlock = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setHideContent(false);
+    }, 2000);
+  };
   return (
     <div className="box">
       {/* <p className="font-semibold">{"Example"}</p> */}
       <div className="mt-4 grid grid-cols-3 gap-4">
         <div className="col-span-2">
-          <SyntaxHighlighter language="js">{code}</SyntaxHighlighter>
+          <SyntaxHighlighter
+            language="js"
+            style={shadesOfPurple}
+            wrapLines
+            showLineNumbers
+            className="rounded px-4 py-2 text-sm"
+          >
+            {INTEGRATION_CODE}
+          </SyntaxHighlighter>
         </div>
         <div className="bg-white relative p-4 rounded pb-16">
           <div>
             <p className="font-semibold text-lg font-serif">{"Web3 Times"}</p>
             <p className="text-sm mt-2 font-serif">{getLorem()}</p>
           </div>
-          <div className="fade rounded" />
-          <div className="absolute bottom-0 py-6 flex items-center justify-center w-full right-0 left-0 bg-white rounded">
-            <button className="btn flex items-center">
-              <img
-                src="/logo-icon-white.png"
-                alt="dyFactor"
-                className="mr-2 h-4"
-              />
-              {"Access with dyFactor Score"}
-            </button>
-          </div>
+          {hideContent ? (
+            <>
+              <div className="fade rounded" />
+              <div className="absolute bottom-0 py-6 flex items-center justify-center w-full right-0 left-0 bg-white rounded">
+                <button className="btn flex items-center" onClick={fakeUnlock}>
+                  {loading ? (
+                    <div className="donutSpinner h-4 w-4 mr-2" />
+                  ) : (
+                    <img
+                      src="/logo-icon-white.png"
+                      alt="dyFactor"
+                      className="mr-2 h-4"
+                    />
+                  )}
+                  {"Access with dyFactor Score"}
+                </button>
+              </div>
+            </>
+          ) : null}
         </div>
       </div>
     </div>
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,16 @@
 export function getLorem() {
   return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id nibh tortor id aliquet lectus proin nibh nisl. Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Massa sed elementum tempus egestas sed sed. Viverra nibh cras pulvinar mattis nunc sed. Nunc consequat interdum varius sit amet int- erdum posuere lorem ipsum dolor sit amet.";
 }
+
+export const INTEGRATION_CODE = `import React from "react";
+
+const DyFactorButton = () => {
+  return (
+    <button className="btn">
+      <img src="/logo-icon-white.png" alt="dyFactor" />
+      {"Access with dyFactor Score"}
+    </button>
+  );
+};
+
+export default DyFactorButton;`;
