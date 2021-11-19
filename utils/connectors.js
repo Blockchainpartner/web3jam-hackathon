4b143c1b85556c5d8df5b103f032b036df6f1247
hanahem <wawa.h8@gmail.com>
2021-11-19 10:58:01 +0100

    :wrench: Updating operable networks

--- utils/connectors.js
+++ utils/connectors.js
@@ -3,18 +3,18 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
 const POLLING_INTERVAL = 12000;
 const RPC_URLS = {
-  1: "https://main-light.eth.linkpool.io/",
   4: "https://rinkeby-light.eth.linkpool.io/",
-  56: "https://bsc-dataseed.binance.org/",
-  250: "https://rpc.ftm.tools/",
+  // 1: "https://main-light.eth.linkpool.io/",
+  // 56: "https://bsc-dataseed.binance.org/",
+  // 250: "https://rpc.ftm.tools/",
 };
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 4, 56, 250],
+  supportedChainIds: [4],
 });
 
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: RPC_URLS[1], 4: RPC_URLS[4], 56: RPC_URLS[56], 250: RPC_URLS[250] },
+  rpc: { 4: RPC_URLS[4] },
   qrcode: true,
 });
 
